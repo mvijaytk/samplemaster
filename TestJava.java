fcd6d0b9d52ef403716b93cfbe41837cc8a0e6ce
Vijaykumar M <vijaykumarm@sterlingsoftware.co.in>
2021-02-17 17:32:40 +0530

    added for CR11

--- TestJava.java
+++ TestJava.java
@@ -1,7 +1,7 @@
 /* Basic code */
 class TestJava{
 public static void main(String args[]){
-for (int i = 0; i <=7; i++) {
+for (int i = 0; i <=7; i++) &&(int i = 1; i <=8; i++) {
   System.out.println("Hello Java");
 	}
 }
