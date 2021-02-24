cd7e0a0a53c1262ae861aa4264fec00a62572630
mvijaytk1 <vijaykumarm@sterlingsoftware.co.in>
2021-02-24 11:26:47 +0530

    Added in diff. line for testing CR6
    To test merge

--- TestJava.java
+++ TestJava.java
@@ -6,3 +6,13 @@ for (int i = 0; i <=7; i++) &&(int i = 1; i <=8; i++)|| (int i = 2; i <=9; i++)|
 	}
 }
 }
+
+/* Added in diff. line*/
+class TestJava{
+public static void main(String args[]){
+for (int i = 0; i <=7; i++) {
+  System.out.println("Hello Java1");
+	}
+}
+}
+
