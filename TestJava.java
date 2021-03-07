bc0fe8aae5e4f278841e37f7d8c64bd3e5810a3f
user1 <vijaykumarm@sterlingsoftware.co.in>
2021-03-07 23:18:21 +0530

    Code added by user1 for CR1

--- TestJava.java
+++ TestJava.java
@@ -6,3 +6,12 @@ for (int i = 0; i <=7; i++) {
 	}
 }
 }
+
+/* Code added by user1 for CR1*/
+class TestJava{
+public static void main(String args[]){
+for (int i = 0; i <=7; i++) {
+  System.out.println("Hello Java CR1");
+	}
+}
+}
