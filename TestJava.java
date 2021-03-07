7bcf324a715f32b27796cce141480ee1b151617a
user2 <vijaykumarm@sterlingsoftware.co.in>
2021-03-07 23:25:43 +0530

    Code added by user2 for CR2

--- TestJava.java
+++ TestJava.java
@@ -6,3 +6,12 @@ for (int i = 0; i <=7; i++) {
 	}
 }
 }
+
+/* Code added by user2 for CR2 */
+class TestJava{
+public static void main(String args[]){
+for (int i = 0; i <=7; i++) {
+  System.out.println("Hello Java CR2");
+	}
+}
+}
